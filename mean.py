c8716c5773860371e102784aaf9146b02a94b810
Yiqi Huang <huangyiqiwilla@gmail.com>
2013-03-08 09:23:32 -0500

    Added initial file to the project.

---
+++ mean.py
@@ -0,0 +1,14 @@
+#!/bin/python 
+###################### second method ######################
+import sys
+
+total=0
+l=0
+for line in sys.stdin:
+        total = total+ float(line.strip())
+        l = l +1
+
+average=total/l
+
+print 'The Average of the Array is',average
+
